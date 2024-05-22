f35d4dc9b08e8998dde93efcb0a15a1bcf5aa53e
nrv28 <122279632+nrv28@users.noreply.github.com>
2024-05-22 15:32:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './client/build', 'index.html'))
 });
 
-const PORT = process.env.PORT || 4000;
+const PORT = 4000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
